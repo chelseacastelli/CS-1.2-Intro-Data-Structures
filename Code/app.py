68c2c0965db6ff3928f1e7791fb4f0fb007033eb
chelseacastelli <chelsea.castelli@students.makeschool.com>
2019-12-02 12:24:14 -0800

    Show random word on page and refresh on button click

--- Code/app.py
+++ Code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 from histogram import read_in_data, dictogram
 from stochastic import random_word
 
@@ -10,7 +10,12 @@ source_text = 'sample_wordz.txt'
 def index():
     words = read_in_data(source_text)
     dict_hist = dictogram(words)
-    return random_word(dict_hist)
+    ran_word = random_word(dict_hist)
+    return render_template('index.html', ran_word=ran_word)
+
+@app.route('/new', methods=['POST'])
+def new_word():
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
