42fef434122ff55358150d7e9853e7ed3bd8421e
chelseacastelli <chelsea.castelli@students.makeschool.com>
2019-12-06 15:46:07 -0800

    [FIX] Num of words button works

--- Tweet-Generator/app.py
+++ Tweet-Generator/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, request, url_for
 from cleanup import read_in_data, clean_text, add_stop, create_sentence
 from random import randint, choice
 from markov_chain import markov_histo, stochastic_sample, random_walk
@@ -15,18 +15,19 @@ markov = markov_histo(stripped_words)
 @app.route('/')
 def index():
 
+    # Get random initial word
     initial_word = choice([word for word in markov.keys() if word != '<STOP>'])
     word = stochastic_sample(markov, initial_word)
-    random_int = randint(5, 15)
-    output = random_walk(word, markov, random_int)
 
+    # Check for user input, otherwise, print sentence w/ 6 words
+    usr_input = request.args.get('num')
+    num_of_words = int(usr_input) if (usr_input != None and usr_input != "") else 6
+
+    # Create sentence with specified num_of_words
+    output = random_walk(word, markov, num_of_words)
     sentence = create_sentence(output)
 
     return render_template('index.html', sentence=sentence)
-
-@app.route('/new', methods=['POST'])
-def new_word():
-    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
