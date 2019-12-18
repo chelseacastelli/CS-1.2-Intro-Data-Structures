b320f09b168c0f6b1c4df1a28f1be61cf11d6b03
chelseacastelli <chelsea.castelli@students.makeschool.com>
2019-12-18 17:02:30 -0500

    [UPDATE] Add higher order markov

--- Tweet-Generator/app.py
+++ Tweet-Generator/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, request, url_for
 from cleanup import read_in_data, clean_text, add_stop, create_sentence
 from random import randint, choice
-from markov_chain import markov_histo, stochastic_sample, random_walk
+from markov_chain import higher_markov, stochastic_sample, random_walk
 
 app = Flask(__name__)
 
@@ -10,21 +10,17 @@ source_text = 'static/tumblr_wordz.txt'
 words = read_in_data(source_text)
 cleaned_text = clean_text(words)
 stripped_words = add_stop(cleaned_text)
-markov = markov_histo(stripped_words)
+markov = higher_markov(stripped_words)
 
 @app.route('/')
 def index():
-
-    # Get random initial word
-    initial_word = choice([word for word in markov.keys() if word != '<STOP>'])
-    word = stochastic_sample(markov, initial_word)
 
     # Check for user input, otherwise, print sentence w/ 6 words
     usr_input = request.args.get('num')
     num_of_words = int(usr_input) if (usr_input != None and usr_input != "") else 6
 
     # Create sentence with specified num_of_words
-    output = random_walk(word, markov, num_of_words)
+    output = random_walk(markov, num_of_words)
     sentence = create_sentence(output)
 
     return render_template('index.html', sentence=sentence)
--- Tweet-Generator/markov_chain.py
+++ Tweet-Generator/markov_chain.py
@@ -1,7 +1,7 @@
 from random import randint, randrange, choice
 from cleanup import read_in_data, clean_text, add_stop
 from dictogram import Dictogram
-
+from queue import Queue
 
 def markov_histo(corpus):
     '''Creates markov chain with histogram'''
@@ -18,6 +18,24 @@ def markov_histo(corpus):
 
     return markov_dict
 
+def higher_markov(corpus):
+    '''Creates 2nd order markov chain with histogram'''
+    markov_dict = {}
+
+    for i in range(len(corpus)-1):
+        first = corpus[i]
+        second = corpus[i+1]
+
+        if second != '<STOP>':
+            third = corpus[i+2]
+
+            key = (first, second)
+            if key not in markov_dict.keys():
+                markov_dict[key] = Dictogram()
+
+            markov_dict.get(key).add_count(third)
+
+    return markov_dict
 
 def stochastic_sample(markov, word):
     '''Gets a weighted random word from given word's histo'''
@@ -26,25 +44,45 @@ def stochastic_sample(markov, word):
     if word in markov:
         return histo.sample()
 
+def random_state(markov):
+    '''Gets states with word as first in tuple'''
+    states = [state for state in markov.keys()]
+    return choice(states)
 
-def random_walk(word, markov, steps):
+
+def random_walk(markov, steps):
     '''Given a starting word, picks a random word from markov list and walks to given number of steps to generate a sentence'''
 
     sentence = []
+    queue = Queue()
 
-    i = 0
+    state = random_state(markov)
+    queue.enqueue(state[0])
+    sentence.append(state[0])
+    queue.enqueue(state[1])
+    sentence.append(state[1])
+
+    i = 2
     while i != steps:
-        sentence.append(word)
-        next_word = stochastic_sample(markov, word)
+        next_word = stochastic_sample(markov, state)
+
+        if len(queue) == 3:
+            queue.dequeue()
+
+        queue.enqueue(next_word)
+        sentence.append(next_word)
+
+        state = (state[1], next_word)
+
         if next_word == '<STOP>':
             break
-        word = next_word
+
         i += 1
 
     return sentence
 
 if __name__ == "__main__":
-    file = 'sample_wordz.txt'
+    file = 'static/tumblr_wordz.txt'
     text = read_in_data(file)
     clean = clean_text(text)
     endstop = add_stop(clean)
